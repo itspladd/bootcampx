68eeb86101e569f61cced390f836360f1284b0c2
Paul Ladd <vlendle@gmail.com>
2021-03-30 18:49:40 +0000

    Add teacher and assignment submission seeds

--- migrations/teachers_assistance_requests.sql
+++ migrations/teachers_assistance_requests.sql
@@ -1,9 +1,9 @@
 CREATE TABLE teachers (
  id SERIAL PRIMARY KEY NOT NULL,
- name VARCHAR(50),
+ name VARCHAR(255) NOT NULL,
  start_date DATE,
  end_date DATE,
- is_active BOOLEAN
+ is_active BOOLEAN DEFAULT TRUE
 );
 
 CREATE TABLE assistance_requests (
